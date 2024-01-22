f08d4eecabee31791d41c9464494dd0d8c1428a7
GroovyHooked <dafrenchie2002@yahoo.fr>
2024-01-22 09:56:14 +0100

    Redux dev tools implementation

--- src/state_management/store.js
+++ src/state_management/store.js
@@ -1,6 +1,13 @@
 import { createStore } from 'redux';
 import reducer from './reducers.js'
 
-const store = createStore(reducer);
+// Enable Redux DevTools Extension
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+
+const store = createStore(
+    reducer,
+    composeEnhancers()
+    );
 
 export default store
